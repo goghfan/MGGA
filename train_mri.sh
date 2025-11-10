2fcf8e5c47cae61fa56fb1064a7b6cffa6bcecbb
gaofan <69027900+goghfan@users.noreply.github.com>
2025-11-10 19:06:26 +0800

    update script of train and test

---
+++ train_mri.sh
@@ -0,0 +1,19 @@
+#!/bin/sh 
+
+#SBATCH -p gpu
+
+#SBATCH -o train_MRI_2.6_out.log
+
+#SBATCH -e train_MRI_2.6_err.log
+
+#SBATCH -J optimize
+
+#SBATCH -N 1
+
+#SBATCH --gpus=1
+
+# 设置新的MASTER_PORT环境变量，确保端口没有被占用
+export MASTER_PORT=29507
+
+torchrun --nproc_per_node=1 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=29507 /data/home/scyb093/GF/MedSAM/demo3.py
+
